25b55dc4b73997728b354642c051a2b69a4623f5
Administrator <Administrator@EC2AMAZ-PND101J>
2023-05-16 18:31:05 +0000

    Add project files.

---
+++ EmpowerSession2NYC_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerSession2NYC_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerSession2NYC_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6030E587-9928-4749-A59B-0CFA85CD3D37")]
+[assembly: AssemblyVersion("1.0.0.0")]
