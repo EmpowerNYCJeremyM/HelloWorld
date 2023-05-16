5471187d502eecaf21590b3cbf5c40a95d7caa33
Administrator <Administrator@EC2AMAZ-PND101J>
2023-05-16 18:38:19 +0000

    Changed the event message again.

--- EmpowerSession2NYC_1/EmpowerSession2NYC_1.cs
+++ EmpowerSession2NYC_1/EmpowerSession2NYC_1.cs
@@ -68,7 +68,7 @@ namespace EmpowerSession2NYC_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("Hello World from NYC!");
 		}
 	}
 }
